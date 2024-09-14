f61c8b34a398a13f3cfd0a69cd470f3a5f1ea6c1
SCRIPTERBLOX <scriptoblox@gmail.com>
2024-09-14 22:43:15 +0100

    TWSRT2

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-rect(100, 100, 200, 200)
+void setup() {
+    size(600, 600);
+    backgroundShade = random(255);
+}
+
+void draw() {
+    background(0);
+
+    rect(mouseX-25, mouseY-25, 50, 50)
+    ellipse(mouseX, mouseY, 20, 20);
+
+    document.getElementById("label").innerHTML = "Mouse coordinates: " + mouseX + ", " + mouseY;
+}
